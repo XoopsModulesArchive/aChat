1ce25dd70c8267e69059c3f5499762ea84c9eb26
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- language/schinese_utf8/admin.php
+++ language/schinese_utf8/admin.php
@@ -39,34 +39,34 @@
 // Menu
 define("_AM_ACHAT_MODULEADMIN","模块管理:");
 
-define("_AM_ACHAT_CREDIT", "aChat 0.2 is an original creation of Niluge_KiWi<br />(c) Jully-Augustus 2006");
+define("_AM_ACHAT_CREDIT","aChat 0.2 is an original creation of Niluge_KiWi<br />(c) Jully-Augustus 2006");
 
 // Home
-define("_AM_ACHAT_WELCOME", "欢迎来到aChat模块管理后台.");
-define("_AM_ACHAT_NBRE_MSG", "数据库中的消息数");
-define("_AM_ACHAT_MESSAGES", "消息");
-define("_AM_ACHAT_EMPTY_FIELD", "请填写表单.");
+define("_AM_ACHAT_WELCOME","欢迎来到aChat模块管理后台.");
+define("_AM_ACHAT_NBRE_MSG","数据库中的消息数");
+define("_AM_ACHAT_MESSAGES","消息");
+define("_AM_ACHAT_EMPTY_FIELD","请填写表单.");
 
 // Permissions
-define("_AM_ACHAT_PERM_CANPOST", "可以发送消息");
+define("_AM_ACHAT_PERM_CANPOST","可以发送消息");
 
 // Purge
-define("_AM_ACHAT_PURGEPERNBRE", "按消息数清除");
-define("_AM_ACHAT_PURGE_HOWMANY", "清除多少条消息?");
-define("_AM_ACHAT_PURGE_CREATELOG", "为被清除的消息创建一个聊天记录档案?");
-define("_AM_ACHAT_PURGE_VALIDATE", "确定要清除&nbsp;");
-define("_AM_ACHAT_PURGE_SUPPR_NOLOG", "不要logfile");
-define("_AM_ACHAT_PURGE_ERROR_WRITEFILE", "创建logfile时出现错误. 请确认在模块参数种设置的文件夹存在，并有合适的权限.");
-define("_AM_ACHAT_PURGE_LOG_WRITTEN", "聊天记录档案已创建.");
-define("_AM_ACHAT_PURGE_CANCELED", "清除动作已取消.");
-define("_AM_ACHAT_PURGE_OK", "清除成功.");
-define("_AM_ACHAT_PURGE_NBREMSGDEL", "已经删除的消息数: ");
-define("_AM_ACHAT_PURGE_ERROR", "清除时出现错误.");
-define("_AM_ACHAT_PURGEPERDATE", "按天数清除");
-define("_AM_ACHAT_PURGE_KEEP_HMDAYS", "保留最近 x 天的消息");
-define("_AM_ACHAT_PURGE_VALIDATE_PERDAY", "在xx之前发表的消息");
-define("_AM_ACHAT_PURGE_VALIDATE_PERDAY2", " last days");
-define("_AM_ACHAT_PURGE_NOMSG", "没有可删除的消息.");
+define("_AM_ACHAT_PURGEPERNBRE","按消息数清除");
+define("_AM_ACHAT_PURGE_HOWMANY","清除多少条消息?");
+define("_AM_ACHAT_PURGE_CREATELOG","为被清除的消息创建一个聊天记录档案?");
+define("_AM_ACHAT_PURGE_VALIDATE","确定要清除&nbsp;");
+define("_AM_ACHAT_PURGE_SUPPR_NOLOG","不要logfile");
+define("_AM_ACHAT_PURGE_ERROR_WRITEFILE","创建logfile时出现错误. 请确认在模块参数种设置的文件夹存在，并有合适的权限.");
+define("_AM_ACHAT_PURGE_LOG_WRITTEN","聊天记录档案已创建.");
+define("_AM_ACHAT_PURGE_CANCELED","清除动作已取消.");
+define("_AM_ACHAT_PURGE_OK","清除成功.");
+define("_AM_ACHAT_PURGE_NBREMSGDEL","已经删除的消息数: ");
+define("_AM_ACHAT_PURGE_ERROR","清除时出现错误.");
+define("_AM_ACHAT_PURGEPERDATE","按天数清除");
+define("_AM_ACHAT_PURGE_KEEP_HMDAYS","保留最近 x 天的消息");
+define("_AM_ACHAT_PURGE_VALIDATE_PERDAY","在xx之前发表的消息");
+define("_AM_ACHAT_PURGE_VALIDATE_PERDAY2"," last days");
+define("_AM_ACHAT_PURGE_NOMSG","没有可删除的消息.");
 
 // Delete Messages
 define("_AM_ACHAT_DELETEMSG","Supprimer un message");
@@ -75,9 +75,9 @@ define("_AM_ACHAT_DELETEMSG_OK","Message num&eacute;ro %u supprim&eacute;.");
 define("_AM_ACHAT_DELETEMSG_ERROR","Une erreur est survenue lors de la suppression du message num閞o %u.");
 
 // Utilities ( Clone ) ( from myHome module )
-define("_AM_ACHAT_CLONE", "模块克隆");
+define("_AM_ACHAT_CLONE","模块克隆");
 
-define("_AM_ACHAT_CLONENAME", "克隆名称<br /><i>
+define("_AM_ACHAT_CLONENAME","克隆名称<br /><i>
                                          <ul>
                                              <li>不要超过16个字符</li>
                                              <li>不能用特殊字符</li>
@@ -90,10 +90,10 @@ define("_AM_ACHAT_SUBMIT",	"克隆!");
 define("_AM_ACHAT_CLEAR",	"删除");
 define("_AM_ACHAT_CANCEL",	"取消");
 
-define("_AM_ACHAT_CLONED", "模块已成功克隆");
-define("_AM_ACHAT_MODULEXISTS", "这个模块已经存在");
-define("_AM_ACHAT_NOTCLONED", "克隆设置有误");
-define("_AM_ACHAT_CLONE_TROUBLE", "您的服务器设置不允许克隆操作.
+define("_AM_ACHAT_CLONED","模块已成功克隆");
+define("_AM_ACHAT_MODULEXISTS","这个模块已经存在");
+define("_AM_ACHAT_NOTCLONED","克隆设置有误");
+define("_AM_ACHAT_CLONE_TROUBLE","您的服务器设置不允许克隆操作.
 					 请在一个可以更改权限的服务器重新测试.
                                          (例如, 本地服务器)");
 ?>
